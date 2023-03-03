89cd27efa9dc044916a6736225846aad41d14ecd
Noura Algohary <noura.malgohary@gmail.com>
2023-03-03 06:42:40 +0200

    add Merge Two Sorted Lists Iterative

---
+++ 3. Merge Two Sorted Lists/Iterative (py).py
@@ -0,0 +1,41 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        if not(list1):
+            return list2
+        
+        if not(list2):
+            return list1
+
+        head = tail = ListNode()
+
+        if list1.val <= list2.val:
+            head = list1
+            list1 = list1.next
+        else:
+            head = list2
+            list2 = list2.next
+
+        tail = head
+
+        while list1 and list2:
+
+            if list1.val <= list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+
+            tail = tail.next
+
+        if list1:
+            tail.next = list1
+        elif list2:
+            tail.next = list2
+
+        return head
