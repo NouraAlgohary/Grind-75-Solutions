24a272dc92277c4eddb0ce4df5c2595f6598d02b
Noura Algohary <noura.malgohary@gmail.com>
2023-03-26 17:22:02 +0200

    add 11

---
+++ 11. Balanced Binary Tree/Recursion (py).py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if root:
+            def depth(node):
+                if node:
+                    left, right = depth(node.left), depth(node.right)
+
+                    if left == -1 or right == -1 or abs(left - right) > 1:
+                        return -1
+                    
+                    return max(left, right) + 1
+                
+                return 0
+            
+            if depth(root) == -1:
+                return False
+            
+        return True
+        
