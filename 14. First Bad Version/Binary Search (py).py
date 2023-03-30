e41b8bb7206856e69ce807c2c87c9ce83fedae79
Noura Algohary <noura.malgohary@gmail.com>
2023-03-30 15:49:07 +0200

    add 14

---
+++ 14. First Bad Version/Binary Search (py).py
@@ -0,0 +1,17 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        low, high = 0, n
+        mid = int(low + high) / 2
+
+        while low < high:
+            if isBadVersion(mid):
+                high = mid
+            else:
+                low = mid + 1
+            
+            mid = int(low + high) / 2
+        
+        return int(high)
